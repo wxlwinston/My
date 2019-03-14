4600f6a8758d3c9001babd747370c82339eb1a7b
wxlwinston <42276594+wxlwinston@users.noreply.github.com>
2019-03-14 18:33:39 +0800

    @wuxl

--- Exercise/src/com/asiainfo/stock/wordCount.java
+++ Exercise/src/com/asiainfo/stock/wordCount.java
@@ -1,9 +1,18 @@
 package com.asiainfo.stock;
 
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
 public class wordCount {
     public static void main(String[] args) {
         System.out.println("请出入您要查询的字符串: ");
-        String string = System.console().readLine();
+        Scanner scan = new Scanner(System.in);
+        String string=scan.next();
         System.out.println("您输入的字符串为: "+string);
+        int i;
+        for(i=0;i<string.length();i++){
+            Array[] array=new Array[string.length()];
+        }
+        System.out.println(i);
     }
 }
