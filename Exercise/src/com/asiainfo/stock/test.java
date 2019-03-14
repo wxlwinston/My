c0a9284c58e209a0b8d0cc72a60dc597cf696386
wxlwinston <42276594+wxlwinston@users.noreply.github.com>
2019-03-14 18:12:43 +0800

    @wuxl

--- Exercise/src/com/asiainfo/stock/test.java
+++ Exercise/src/com/asiainfo/stock/test.java
@@ -2,6 +2,16 @@ package com.asiainfo.stock;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("dsafasf");
+        String s ="asdas@sdfasdf.com";
+        System.out.println(s);
+        int location = s.indexOf("@");
+        String c=s.substring(0,location);
+        System.out.println(c);
+        Boolean a="".equals(s);
+        if(a){
+            System.out.println(111);
+        }else{
+            System.out.println(222);
+        }
     }
 }
---
+++ Exercise/src/com/asiainfo/stock/wordCount.java
@@ -0,0 +1,9 @@
+package com.asiainfo.stock;
+
+public class wordCount {
+    public static void main(String[] args) {
+        System.out.println("请出入您要查询的字符串: ");
+        String string = System.console().readLine();
+        System.out.println("您输入的字符串为: "+string);
+    }
+}
